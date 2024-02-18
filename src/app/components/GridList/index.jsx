16ae3e9a861c3511ac4fad5a1726a8aa4f95ee23
Thiago <thiagofreitas1106@gmail.com>
2024-02-18 13:50:26 -0300

    added sprites on datagrid mode

--- src/app/components/GridList/index.jsx
+++ src/app/components/GridList/index.jsx
@@ -1,25 +1,49 @@
 import { Box } from "@mui/material";
 import { DataGrid, ptBR } from "@mui/x-data-grid";
+import Image from "next/image";
 
-const TableList = ({ pokemonList, loading, page, itemsPerPage, totalPages, onPageChange, onPageSizeChange }) => {
-
+const TableList = ({
+  pokemonList,
+  loading,
+  page,
+  itemsPerPage,
+  totalPages,
+  onPageChange,
+  onPageSizeChange,
+}) => {
   const columns = [
-    { field: "id", headerName: "ID", width: 90 },
-    { field: "name", headerName: "Name", width: 150 },
-    { field: "type", headerName: "Type", width: 150 },
-    { field: "height", headerName: "Height", width: 110 },
-    { field: "weight", headerName: "Weight", width: 110 },
+    {
+      field: "id",
+      headerName: "Código",
+      width: 100,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "sprite",
+      headerName: "Pokemon",
+      width: 150,
+      renderCell: (params) => (
+        <Image
+          src={params.row.sprites.other["official-artwork"].front_default}
+          width={50}
+          height={50}
+        />
+      ),
+    },
+    { field: "name", headerName: "Nome", width: 150 },
+    { field: "height", headerName: "Tamanho", width: 150 },
+    { field: "weight", headerName: "Peso", width: 150 },
   ];
 
   return (
     <Box p={5} style={{ width: "100%" }}>
       <DataGrid
-      localeText={
-        ptBR.components.MuiDataGrid.defaultProps.localeText
-      }
+        localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
         rows={pokemonList}
         rowHeight={75}
         autoHeight
+        page={page}
         columns={columns}
         pageSize={itemsPerPage}
         onPageChange={onPageChange}
